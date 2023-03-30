2f9bc81bb88235ec8e30f8c1713c50140e0680e9
Nicolas Wendling <contact@nicolaswendling.com>
2023-03-30 09:21:49 +0200

    Start PoC

---
+++ src/components/create-destination-wallet.jsx
@@ -0,0 +1,42 @@
+import {useCreateWallet, Wallet, WalletAddress} from "@nice-xrpl/react-xrpl"
+import {useState} from "react"
+
+export function CreateDestinationWallet({children}) {
+  const [address, setAddress] = useState("")
+  const [sending, setSending] = useState(false)
+
+  // When connected to the testnet/dev net, you can
+  // use the useCreateWallet series of hooks to create
+  // a wallet and fund it from the faucet.
+  const createWallet = useCreateWallet()
+
+  // The Wallet component is used when you have
+  // credentials. It enables the use of all
+  // transactional hooks and all request hooks.
+  return address ? (
+    <WalletAddress address={address}>{children}</WalletAddress>
+  ) : (
+    <div>
+      {!sending ? (
+        <button
+          className="p-2 bg-blue-500 text-white rounded"
+          onClick={async () => {
+            setSending(true)
+            const initialState = await createWallet("1048")
+
+            setSending(false)
+
+            if (initialState.wallet.address) {
+              console.log("created wallet: ", initialState)
+              setAddress(initialState.wallet.address)
+            }
+          }}
+        >
+          Create destination wallet
+        </button>
+      ) : (
+        "Creating destination wallet..."
+      )}
+    </div>
+  )
+}
