adab57882933c9bb4c560ab0fae6744112474d2b
Nicolas Wendling <contact@nicolaswendling.com>
2023-03-30 21:36:20 +0200

    Slider range

--- src/components/create-source-wallet.jsx
+++ src/components/create-source-wallet.jsx
@@ -3,12 +3,19 @@ import {useState} from "react"
 import {Number} from "./wallet-ui/number"
 
 export function CreateSourceWallet({children}) {
-  const min = 100
+  const min = 1000
   const max = 1000000
   const step = 1000
   const [seed, setSeed] = useState("")
   const [sending, setSending] = useState(false)
   const [amount, setAmount] = useState(min)
+
+  const handlerAdjustAmount = (event) => {
+    const value = parseInt(event.currentTarget.value, 10)
+    if (value >= min && value <= max) {
+      setAmount(value)
+    }
+  }
 
   // When connected to the testnet/dev net, you can
   // use the useCreateWallet series of hooks to create
@@ -23,35 +30,41 @@ export function CreateSourceWallet({children}) {
   ) : (
     <div>
       {!sending ? (
-        <div className="flex gap-2">
+        <>
+          <div className="flex gap-2">
+            <input
+              type="number"
+              min={min}
+              max={max}
+              step={step}
+              value={amount}
+              className="p-4 border-blue-950 w-full border-2 rounded-md"
+              onChange={handlerAdjustAmount}
+            />
+            <button
+              className="p-4 bg-blue-950 text-white rounded w-full hover:bg-blue-900 transition-colors duration-300"
+              onClick={async () => {
+                setSending(true)
+                const initialState = await createWallet(amount.toString())
+                setSending(false)
+                if (initialState.wallet.seed) {
+                  setSeed(initialState.wallet.seed)
+                }
+              }}
+            >
+              Create wallet of <Number value={amount} /> XRP
+            </button>
+          </div>
           <input
-            type="number"
+            type="range"
+            className="w-full mt-4"
             min={min}
             max={max}
             step={step}
             value={amount}
-            className="p-4 border-blue-950 w-full border-2 rounded-md"
-            onChange={(input) => {
-              const value = parseInt(input.currentTarget.value, 10)
-              if (value >= min && value <= max) {
-                setAmount(value)
-              }
-            }}
+            onChange={handlerAdjustAmount}
           />
-          <button
-            className="p-4 bg-blue-950 text-white rounded w-full hover:bg-blue-900 transition-colors duration-300"
-            onClick={async () => {
-              setSending(true)
-              const initialState = await createWallet(amount.toString())
-              setSending(false)
-              if (initialState.wallet.seed) {
-                setSeed(initialState.wallet.seed)
-              }
-            }}
-          >
-            Create wallet of <Number value={amount} /> XRP
-          </button>
-        </div>
+        </>
       ) : (
         <div className="p-4 text-center">Creating wallet...</div>
       )}
