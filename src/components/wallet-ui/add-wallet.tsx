ba74eefdf94f5344e1cabb87d2b32b53f34971f2
Nicolas Wendling <contact@nicolaswendling.com>
2023-03-31 12:44:09 +0200

    JSX to TSX

---
+++ src/components/wallet-ui/add-wallet.tsx
@@ -0,0 +1,19 @@
+export const AddWallet = ({
+  onClick,
+  disabled,
+}: {
+  onClick: () => {}
+  disabled: boolean
+}) => {
+  return (
+    <div className="mt-4 text-center">
+      <button
+        disabled={disabled}
+        className="px-6 py-4 transition-colors duration-500 bg-blue-600 rounded-md text-blue-50 hover:bg-blue-200 hover:text-blue-950"
+        onClick={onClick}
+      >
+        Add a new Wallet
+      </button>
+    </div>
+  )
+}
