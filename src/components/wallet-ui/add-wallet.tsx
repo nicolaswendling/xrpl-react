3c9c672f9023d86a38327fc653002f4b0145424d
Nicolas Wendling <contact@nicolaswendling.com>
2023-03-31 15:38:30 +0200

    Disable button

--- src/components/wallet-ui/add-wallet.tsx
+++ src/components/wallet-ui/add-wallet.tsx
@@ -8,7 +8,7 @@ export const AddWallet = ({onClick, disabled}: Props) => {
     <div className="mt-4 text-center">
       <button
         disabled={disabled}
-        className="px-6 py-4 transition-colors duration-500 bg-blue-600 rounded-md text-blue-50 hover:bg-blue-200 hover:text-blue-950"
+        className="px-6 py-4 transition-colors duration-500 bg-blue-600 rounded-md text-blue-50 hover:bg-blue-200 hover:text-blue-950 disabled:bg-slate-800 disabled:text-slate-600 disabled:pointer-events-none"
         onClick={onClick}
       >
         Add a new Wallet
