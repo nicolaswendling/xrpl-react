4ac60b022c0cce17f6b4ef293abe8091cdb93ec0
Nicolas Wendling <contact@nicolaswendling.com>
2023-03-31 12:45:09 +0200

    Limit wallets

--- src/MainApp.jsx
+++ src/MainApp.jsx
@@ -4,15 +4,22 @@ import {AddWallet} from "./components/wallet-ui/add-wallet.jsx"
 import {Connected} from "./components/connected.jsx"
 
 export const MainApp = () => {
+  const MAX_WALLETS = 4
   const [wallets, setWallets] = useState(2)
-  const addWallet = () => setWallets((current) => current + 1)
+  const addWallet = () => {
+    if (wallets >= MAX_WALLETS)
+      return alert(`You can only have ${MAX_WALLETS} wallets at a time`)
+    setWallets((current) => current + 1)
+  }
 
   return (
     <main className="font-mono">
-      {[...Array(wallets)].map((_, index) => (
-        <Wallet key={`wallet_${index}`} id={`wallet_${index}`} />
-      ))}
-      <AddWallet onClick={addWallet} />
+      <div className="grid gap-4">
+        {[...Array(wallets)].map((_, index) => (
+          <Wallet key={`wallet_${index}`} id={`wallet_${index}`} />
+        ))}
+      </div>
+      <AddWallet disabled={wallets >= MAX_WALLETS} onClick={addWallet} />
       <Connected />
     </main>
   )
