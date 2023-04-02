989f9efecbdffab573283380b0d39bc4f63a509c
Nicolas Wendling <contact@nicolaswendling.com>
2023-04-02 09:33:37 +0200

    Style modal

--- src/components/dialog/Modal.tsx
+++ src/components/dialog/Modal.tsx
@@ -11,11 +11,13 @@ export const Modal = ({handleCloseModal, title, children}: ModalProps) => {
       id="modal"
       className="fixed inset-0 z-50 flex items-center justify-center w-screen h-screen p-4 bg-slate-950 bg-opacity-70 backdrop-blur-sm"
     >
-      <div className="flex flex-col w-full max-w-xl overflow-hidden p-4 bg-white bg-gradient-to-tl from-slate-300 min-h-[350px] rounded-md">
-        <header className="p-4 text-center">
-          <h2 className="mb-2 text-2xl font-semibold">{title}</h2>
+      <div className="flex flex-col w-full max-w-xl overflow-hidden px-4 bg-white bg-gradient-to-tl from-slate-300 min-h-[350px] rounded-md">
+        <header className="p-8 text-center">
+          <h2 className="text-2xl font-bold leading-none text-blue-600">
+            {title}
+          </h2>
         </header>
-        <main className="flex items-center flex-1 px-8 py-8 rounded-md shadow-xl bg-slate-50">
+        <main className="flex items-center flex-1 px-8 py-8 rounded-md bg-slate-50">
           <div className="m-auto">{children}</div>
         </main>
         <footer className="flex justify-center p-4">
