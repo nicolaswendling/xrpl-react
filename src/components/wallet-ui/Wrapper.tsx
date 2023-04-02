eb357215b71297c50e82505774bf51ba09d08669
Nicolas Wendling <contact@nicolaswendling.com>
2023-04-02 12:58:30 +0200

    Height loading Create Wallet

--- src/components/wallet-ui/Wrapper.tsx
+++ src/components/wallet-ui/Wrapper.tsx
@@ -3,7 +3,7 @@ type Props = ComponentPropsWithoutRef<"div">
 
 export const Wrapper = ({children}: Props) => {
   return (
-    <div className="w-full p-4 bg-white rounded-md shadow-md bg-gradient-to-tl min-h-[136px] from-slate-300 shadow-slate-950">
+    <div className="w-full p-4 bg-white rounded-md shadow-md bg-gradient-to-tl min-h-[195px] md:min-h-[136px] from-slate-300 shadow-slate-950">
       {children}
     </div>
   )
